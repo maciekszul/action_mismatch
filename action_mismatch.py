234005a8d76f0a0068441fb2e9c5185b375dfdbe
Maciek Szul <maciek.pwns@gmail.com>
2019-01-30 16:36:20 +0000

    NEW: exp test file

--- action_mismatch.py
+++ action_mismatch.py
@@ -1,1 +1,48 @@
-# exp
+# exp dev
+
+from psychopy import core
+from psychopy import visual
+from psychopy import monitors
+from psychopy import gui
+from psychopy import event
+from psychopy.iohub import launchHubServer
+
+import numpy as np
+
+
+# monitor settings
+x230 = (17.26, 56, (1366, 768))
+
+width, dist, res = x230
+
+mon = monitors.Monitor("default")
+mon.setWidth(width)
+mon.setDistance(dist)
+mon.setSizePix(res)
+
+win = visual.Window(size=res,
+                    color="#000000",
+                    fullscr=True,
+                    allowGUI=False,
+                    winType="pyglet",
+                    units="deg",
+                    gamma=0,
+                    monitor=mon)
+
+# stim
+text = visual.TextStim(win, text=" ",
+                        color="white",
+                        units="deg",
+                        height=1,
+                        opacity=1.0,
+                        name="cross",
+                        alignHoriz="center",
+                        alignVert="center")
+
+text.text = "blabla"
+text.draw()
+win.flip()
+core.wait(2)
+
+# win.close()
+core.quit()
