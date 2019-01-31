d9d7c5bb077040915b1c302e0bfab88aff6d9dc3
Maciek Szul <maciek.pwns@gmail.com>
2019-01-31 15:58:53 +0000

    FIX: cosmetic changes

--- exp_prototype.py
+++ exp_prototype.py
@@ -83,7 +83,11 @@ def draw_cue():
 # stim
 text_stim = visual.TextStim(
     win,
-    text=""
+    text="",
+    height=0.5,
+    color="#808080",
+    pos=(-28, 14.5),
+    alignHoriz="left"
 )
 outer = visual.Circle(win, 10, edges=100)
 line = visual.Line(win, start=(0.0, 0.0), end=(10.0, 0.0))
@@ -106,25 +110,33 @@ check = False
 event.clearEvents()
 
 while not event.getKeys(keyList=['q'], timeStamped=False):
+    
     if event.getKeys(keyList=['i'], timeStamped=False):
         check = True
     elif event.getKeys(keyList=['c'], timeStamped=False):
         check = False
+    
     x, y = joy.getX(), -joy.getY()
+    
     theta, radius = ct.cart2pol(x, y, units="rad")
     theta_delta = theta-theta0
-    text_stim.text = "X:{}\nY: {}\nANG: {}\nRAD: {}\ndelta ANG: {}".format(x,y, theta+np.pi, radius, theta_delta)
-    text_stim.draw()
+    
     if check:
-        theta_start = theta_start - (theta_delta)
+        theta_start = theta_start - (theta_delta ) # oddball conditions
     else:
-        theta_start = theta_start + (theta_delta)
+        theta_start = theta_start + (theta_delta) # regular
+    
     ball.pos = ct.pol2cart(theta_start, 10, units="rad")
     ball.draw()
+
+    outer.draw()
+
+    text_stim.text = "X:{}\nY: {}\nANG: {}\nRAD: {}\ndelta ANG: {}".format(x,y, theta+np.pi, radius, theta_delta)
+    text_stim.draw()
+    
     win.flip()
+    
     theta0 = theta
-
-
 
 win.close()
 core.quit()
